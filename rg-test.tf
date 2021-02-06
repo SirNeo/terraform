7086a32208530725790472e31fd5af28766a3704
José María Palma <5236294+SirNeo@users.noreply.github.com>
2021-02-06 17:47:33 +0100

    added provider

--- rg-test.tf
+++ rg-test.tf
@@ -1,3 +1,17 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "rg-terraform-github-test"
   location = "West Europe"
